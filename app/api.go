bbfdfa510a0e95be9513920599e79724ccef1a14
Peppy Sisay <peppysisay@gmail.com>
2018-12-02 02:17:49 -0800

    CORS

--- app/api.go
+++ app/api.go
@@ -1,17 +1,22 @@
 package app
 
 import (
-	"time"
 	"net/http"
+	"time"
+
+	"github.com/PeppyS/api.peppysisay.com/app/middleware"
 
 	"github.com/gin-gonic/gin"
 )
+
 type API struct {
 	router *gin.Engine
 }
 
 func NewAPI() *API {
 	r := gin.Default()
+
+	r.Use(middleware.CORSMiddleware())
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -20,19 +25,23 @@ func NewAPI() *API {
 	})
 
 	p := r.Group("/post")
-	
-	p.GET("/:id/comments", func(c * gin.Context) {
+
+	p.GET("/:id", func(c *gin.Context) {
 		postID := c.Param("id")
 
 		c.JSON(http.StatusOK, gin.H{
 			"success": true,
-			"data": []gin.H{
-				gin.H{
-					"post_id": postID,
-					"created_at": time.Now().String(),
-					"first_name": "Bob",
-					"last_name": "Smith",
-					"text": "Great write-up! Keep up the good work.",
+			"data": gin.H{
+				"likes_count": 0,
+				"comments": []gin.H{
+					gin.H{
+						"post_id":     postID,
+						"created_at":  time.Now().String(),
+						"first_name":  "Bob",
+						"last_name":   "Smith",
+						"text":        "Great write-up! Keep up the good work.",
+						"likes_count": "0",
+					},
 				},
 			},
 		})
